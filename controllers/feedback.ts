c89eed3ea1c7fca22fe552fd0e5b66ea486972e6
koraphat <koraphat.lum@gmail.com>
2024-04-06 18:33:14 +0700

    fix some text

--- controllers/feedback.ts
+++ controllers/feedback.ts
@@ -98,7 +98,7 @@ const addFeedback = async (req: Request, res: Response) => {
     if (!CWSpace) {
       return res.status(404).json({
         success: false,
-        message: `No co-working-space with the id of ${cwsid}`,
+        message: `No co-working space with the id of ${cwsid}`,
       });
     }
 
@@ -111,10 +111,10 @@ const addFeedback = async (req: Request, res: Response) => {
     if (existedFeedback.length >= 1 && req.user.role !== "admin") {
       return res.status(400).json({
         success: false,
-        message: `The user with id ${req.user.id} has already made a feedback to co-working-space with id ${cwsid}`,
+        message: `The user with id ${req.user.id} has already made a feedback to co-working space with id ${cwsid}`,
       });
     }
-    console.log(req.body.feedbackString);
+    // console.log(req.body.feedbackString);
     const newFeedback = await Feedback.create(req.body);
 
     res.status(200).json({
