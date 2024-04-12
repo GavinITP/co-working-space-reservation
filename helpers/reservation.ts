f7d37eca2757d9039b0c933ae1c33c7745f4999d
GavinITP <86824319+GavinITP@users.noreply.github.com>
2024-04-12 14:43:19 +0700

    refactor: seperate time validate function

--- helpers/reservation.ts
+++ helpers/reservation.ts
@@ -8,53 +8,68 @@ import {
   getSeconds,
 } from "date-fns";
 
+const isStartTimeValid = (
+  coWorkingSpace: CoWorkingSpaceType,
+  startTime: string
+): boolean => {
+  if (!startTime) return false;
+
+  const reservationStartDateTime = parse(startTime, "HH:mm:ss", new Date());
+  const openingTime = parse(coWorkingSpace.openingTime, "HH:mm:ss", new Date());
+
+  const conditionOne =
+    isAfter(reservationStartDateTime, openingTime) ||
+    isEqual(reservationStartDateTime, openingTime);
+
+  return (
+    conditionOne &&
+    getMinutes(reservationStartDateTime) % 30 === 0 &&
+    getSeconds(reservationStartDateTime) === 0
+  );
+};
+
+const isEndTimeValid = (
+  coWorkingSpace: CoWorkingSpaceType,
+  endTime: string
+): boolean => {
+  if (!endTime) return false;
+
+  const reservationEndDateTime = parse(endTime, "HH:mm:ss", new Date());
+  const closingTime = parse(coWorkingSpace.closingTime, "HH:mm:ss", new Date());
+
+  const conditionTwo =
+    isBefore(reservationEndDateTime, closingTime) ||
+    isEqual(reservationEndDateTime, closingTime);
+
+  return (
+    conditionTwo &&
+    getMinutes(reservationEndDateTime) % 30 === 0 &&
+    getSeconds(reservationEndDateTime) === 0
+  );
+};
+
+const isStartTimeBeforeEndTime = (
+  startTime: string,
+  endTime: string
+): boolean => {
+  if (!startTime || !endTime) return false;
+
+  const reservationStartDateTime = parse(startTime, "HH:mm:ss", new Date());
+  const reservationEndDateTime = parse(endTime, "HH:mm:ss", new Date());
+
+  return isBefore(reservationStartDateTime, reservationEndDateTime);
+};
+
 const validateReservationTime = (
   coWorkingSpace: CoWorkingSpaceType,
   startTime: string,
   endTime: string
 ): boolean => {
-  let conditionOne = true;
-  let conditionTwo = true;
-
-  if (startTime) {
-    const reservationStartDateTime = parse(startTime, "HH:mm:ss", new Date());
-    const openingTime = parse(
-      coWorkingSpace.openingTime,
-      "HH:mm:ss",
-      new Date()
-    );
-
-    conditionOne =
-      isAfter(reservationStartDateTime, openingTime) ||
-      isEqual(reservationStartDateTime, openingTime);
-
-    // Check if reservation start time minutes are only 00 or 30 and seconds are 00
-    conditionOne =
-      conditionOne &&
-      getMinutes(reservationStartDateTime) % 30 === 0 &&
-      getSeconds(reservationStartDateTime) === 0;
-  }
-
-  if (endTime) {
-    const reservationEndDateTime = parse(endTime, "HH:mm:ss", new Date());
-    const closingTime = parse(
-      coWorkingSpace.closingTime,
-      "HH:mm:ss",
-      new Date()
-    );
-
-    conditionTwo =
-      isBefore(reservationEndDateTime, closingTime) ||
-      isEqual(reservationEndDateTime, closingTime);
-
-    // Check if reservation end time minutes are only 00 or 30 and seconds are 00
-    conditionTwo =
-      conditionTwo &&
-      getMinutes(reservationEndDateTime) % 30 === 0 &&
-      getSeconds(reservationEndDateTime) === 0;
-  }
-
-  return conditionOne && conditionTwo;
+  return (
+    isStartTimeValid(coWorkingSpace, startTime) &&
+    isEndTimeValid(coWorkingSpace, endTime) &&
+    isStartTimeBeforeEndTime(startTime, endTime)
+  );
 };
 
 export default validateReservationTime;
